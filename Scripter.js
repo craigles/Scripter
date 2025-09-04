ae67c8f59625f6139dca6d4e0139134a7356ef97
Craig Curtis <ccurtis@laingorourke.com.au>
2025-09-05 08:44:28 +1000

    Refactor riffs to use Songs function for MIDI handling

--- Scripter.js
+++ Scripter.js
@@ -1,33 +1,36 @@
-var riffs = {};
-riffs[Pitch.C4] = [
-	[
-		new Note(Pitch.E4, Length.Double),
-		new Note(Pitch.A4, Length.Whole),
-	],
-	new Note(Pitch.Ab4, Length.Whole)
-];
-riffs[Pitch.B3] = [
-	[
-		new Note(Pitch.D4, Length.Whole),
-		new Note(Pitch.A3, Length.Whole),
-		new Note(Pitch.D3, Length.Whole)
-	],
-	[
-		new Note(Pitch.Db4, Length.Whole),
-		new Note(Pitch.Ab3, Length.Whole),
-		new Note(Pitch.Db3, Length.Whole)
-	],
-];
+function Songs() {
+	return {
+		"C4": [
+			[
+				new Note("E3", Length.Double),
+				new Note("A3", Length.Whole),
+			],
+			new Note("G#3", Length.Whole),
+		],
+		"B3": [
+			[
+				new Note("D3", Length.Whole),
+				new Note("A2", Length.Whole),
+				new Note("D2", Length.Whole)
+			],
+			[
+				new Note("C#3", Length.Whole),
+				new Note("G#2", Length.Whole),
+				new Note("C#2", Length.Whole)
+			],
+		]
+	};
+};
 
 function HandleMIDI(event)
 {
-	var notes = riffs[event.pitch];
+	var notes = Songs()[MIDI.noteName(event.pitch)];
 
 	if (notes) {
 		new Sequencer(notes).play(event.beatPos);
-		return;
+		return
 	}
-
+	
 	event.send();
 }
 
@@ -42,16 +45,16 @@ class Note
 	play(beat)
 	{
 		var noteOn = new NoteOn;
-		noteOn.pitch = this.pitch;
+		noteOn.pitch = MIDI.noteNumber(this.pitch);
 		noteOn.sendAtBeat(beat);
 
 		var noteOff = new NoteOff(noteOn);
-		noteOff.sendAtBeat(beat + this.beatDuration());
+		noteOff.sendAtBeat(beat + this.beatDuration() - GetParameter("Release"));
 	}
 
 	beatDuration()
 	{
-		return this.length * GetTimingInfo().meterDenominator;
+		return this.length * GetTimingInfo().meterNumerator;
 	}
 }
 
@@ -86,54 +89,14 @@ var Length = {
 	Sixteenth: 1 / 16,
 };
 
-var Pitch = {
-	C5: 72,
-	B4: 71,
-	Bb4: 70,
-	A4: 69,
-	Ab4: 68,
-	G4: 67,
-	Gb4: 66,
-	F4: 65,
-	E4: 64,
-	Eb4: 63,
-	D4: 62,
-	Db4: 61,
-	C4: 60,
-	B3: 59,
-	Bb3: 58,
-	A3: 57,
-	Ab3: 56,
-	G3: 55,
-	Gb3: 54,
-	F3: 53,
-	E3: 52,
-	Eb3: 51,
-	D3: 50,
-	Db3: 49,
-	C3: 48,
-	B2: 47,
-	Bb2: 46,
-	A2: 45,
-	Ab2: 44,
-	G2: 43,
-	Gb2: 42,
-	F2: 41,
-	E2: 40,
-	Eb2: 39,
-	D2: 38,
-	Db2: 37,
-	C2: 36
-}
-
 var NeedsTimingInfo = true
 var PluginParameters = [
 	{
 		name: "Release", 
-		defaultValue: 100, 
+		defaultValue: 0.5, 
 		minValue: 0, 
-		maxValue: 1000, 
-		numberOfSteps: 100,
+		maxValue: 1, 
+		numberOfSteps: 10,
 		type: "lin"
 	}
 ];
